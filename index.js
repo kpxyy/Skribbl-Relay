a0539eec34c13ca4500187f0be102bd74ebe9b24
w0ahL <149548505+w0ahL@users.noreply.github.com>
2023-12-09 10:49:11 -0500

    Icons & Bug Fixes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 
 let { WEBHOOK_URL: URL } = process.env;
 
+require("./server.js");
+
 if (!URL) {
   console.error("You must include a URL in the .env file.");
   process.exit(0);
@@ -22,62 +24,73 @@ send({
 
 function main() {
   const client = new Client({
-    name: "Skribbl Relay"
+    name: "܂ցց᜵pegwU7PNxx",
   });
 
   client.on("connect", () => {
     send({
       username: "Skribbl-Relay",
-      embeds: [{
-        title: "System",
-        description: "Relay connected",
-        color: 65280,
-        footer: {
-          text: `Lobby ID: ${client.lobbyId} - Online Players: ${client.players.length} - Skribbl-Relay`
-        }
-      }]
+      embeds: [
+        {
+          title: "System",
+          description: "Relay connected",
+          color: 65280,
+          footer: {
+            text: `Lobby ID: ${client.lobbyId} - Online Players: ${client.players.length} - Skribbl-Relay`,
+          },
+        },
+      ],
     });
 
-    console.log(`Connected to ${client.lobbyId}\nOnline Players: ${client.players.length}`);
-  })
+    console.log(
+      `Connected to ${client.lobbyId}\nOnline Players: ${client.players.length}`,
+    );
+  });
 
-  client.on("votekick", ({voter, votee, currentVotes, requiredVotes}) => {
+  client.on("votekick", ({ voter, votee, currentVotes, requiredVotes }) => {
     send({
       username: "Skribbl-Relay",
-      embeds: [{
-        title: "System",
-        description: `${voter.name} is voting to kick ${votee.name} (${currentVotes}/${requiredVotes})`,
-        color: 14863104
-      }]
+      embeds: [
+        {
+          title: "System",
+          description: `${voter.name} is voting to kick ${votee.name} (${currentVotes}/${requiredVotes})`,
+          color: 14863104,
+          footer: {
+            text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`,
+          },
+        },
+      ],
     });
-  })
+  });
 
   client.on("disconnect", (disconnectData) => {
     console.log(disconnectData);
 
     setTimeout(() => {
       main();
-    }, 5000)
+    }, 1660);
 
     const disconnectedParams = {
       username: "Skribbl-Relay",
-      embeds: [{
-        title: "System",
-        description: "",
-        color: 16711680
-      }]
-    }
+      embeds: [
+        {
+          title: "System",
+          description: "",
+          color: 16711680,
+        },
+      ],
+    };
 
-    switch(disconnectData.reason) {
+    switch (disconnectData.reason) {
       case 1:
         disconnectedParams.embeds[0].description = "Relay kicked";
         break;
 
       case 2:
         disconnectedParams.embeds[0].description = "Relay banned";
-    };
+    }
 
-    switch(disconnectData.joinErr) {
+    switch (disconnectData.joinErr) {
       case 1:
         disconnectedParams.embeds[0].description = "Room not found";
         break;
@@ -107,40 +120,47 @@ function main() {
         break;
 
       case 300:
-        disconnectedParams.embeds[0].description = "Relay kicked too many times";
+        disconnectedParams.embeds[0].description =
+          "Relay kicked too many times";
         break;
 
       case 9999:
-        disconnectedParams.embeds[0].description = "Relay was spamming too many messages.";
+        disconnectedParams.embeds[0].description =
+          "Relay was spamming too many messages.";
         break;
 
       default:
-        if(!disconnectData.joinErr) break;
+        if (!disconnectData.joinErr) break;
 
         disconnectedParams.embeds[0].description = `Undefined if joinErr call: ${disconnectData.joinErr}`;
-    };
+    }
 
-    switch(disconnectData.transportDisconnectReason) {
+    switch (disconnectData.transportDisconnectReason) {
       case "io client disconnect":
-        disconnectedParams.embeds[0].description = "Relay was disconnected by client";
+        disconnectedParams.embeds[0].description =
+          "Relay was disconnected by client";
         break;
 
       case "io server disconnect":
-        disconnectedParams.embeds[0].description = "Relay was disconnected by server";
+        disconnectedParams.embeds[0].description =
+          "Relay was disconnected by server";
         break;
 
       case "transport close":
-        disconnectedParams.embeds[0].description = "Relay was disconnected due to a transport close";
+        disconnectedParams.embeds[0].description =
+          "Relay was disconnected due to a transport close";
         break;
 
       case "transport error":
-        disconnectedParams.embeds[0].description = "Relay was disconnected due to a transport error";
+        disconnectedParams.embeds[0].description =
+          "Relay was disconnected due to a transport error";
         break;
 
       case "ping timeout":
-        disconnectedParams.embeds[0].description = "Relay was disconnected due to a ping timeout";
+        disconnectedParams.embeds[0].description =
+          "Relay was disconnected due to a ping timeout";
         break;
-    };
+    }
 
     send(disconnectedParams);
   });
@@ -152,78 +172,92 @@ function main() {
   client.on("playerJoin", (userJoin) => {
     send({
       username: "Skribbl-Relay",
-      embeds: [{
-        title: "System",
-        description: `**${userJoin.name}** joined the room!`,
-        color: 65280,
-        footer: {
-          text: `Lobby ID: ${client.lobbyId} - Online Players: ${client.players.length} - Skribbl-Relay`
-        }
-      }]
+      embeds: [
+        {
+          title: "System",
+          description: `**${userJoin.name}** joined the room!`,
+          color: 65280,
+          footer: {
+            text: `Lobby ID: ${client.lobbyId} - Online Players: ${client.players.length} - Skribbl-Relay`,
+          },
+        },
+      ],
     });
   });
 
-  client.on("playerLeave", ({player, reason}) => {
-        const message = {
-            username: "Skribbl-Relay",
-            embeds: [{
-                title: "System",
-                description: "",
-                color: 16711680,
-                footer: {
-                    text: `Lobby ID: ${client.lobbyId} - Online Players: ${client.players.length} - Skribbl-Relay`
-                }
-            }]
-        };
+  client.on("playerLeave", ({ player, reason }) => {
+    const message = {
+      username: "Skribbl-Relay",
+      embeds: [
+        {
+          title: "System",
+          description: "",
+          color: 16711680,
+          footer: {
+            text: `Lobby ID: ${client.lobbyId} - Online Players: ${client.players.length} - Skribbl-Relay`,
+          },
+        },
+      ],
+    };
 
-        switch(reason) {
-            case 0:
-                message.embeds[0].description = `**${player.name}** left the room!`;
-                break;
+    switch (reason) {
+      case 0:
+        message.embeds[0].description = `**${player.name}** left the room!`;
+        break;
 
-            case 1:
-                message.embeds[0].description = `**${player.name}** has been kicked.`;
-                break;
+      case 1:
+        message.embeds[0].description = `**${player.name}** has been kicked.`;
+        break;
 
-            case 2:
-                message.embeds[0].description = `**${player.name}** has been banned.`;
-                break;
-        }
+      case 2:
+        message.embeds[0].description = `**${player.name}** has been banned.`;
+        break;
+    }
 
-        send(message);
+    send(message);
   });
 
-  client.on("newOwner", ({player}) => {
+  client.on("newOwner", ({ player }) => {
     send({
       username: "Skribbl-Relay",
-      embeds: [{
-        title: "System",
-        description: `**${player.name}** is now the new host.`,
-        color: 16754756,
-        footer: {
-          text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`
-        }
-      }]
+      embeds: [
+        {
+          title: "System",
+          description: `**${player.name}** is now the new host.`,
+          color: 16754756,
+          footer: {
+            text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`,
+          },
+          thumbnail: {
+            url: "https://skribbl.io/img/crown.gif",
+          },
+        },
+      ],
     });
   });
 
   // packet logging for id 11
-  client.on("packet", ({id, data}) => {
+  client.on("packet", ({ id, data }) => {
     if (id != 11) return;
 
     const message = {
       username: "Skribbl-Relay",
-      embeds: [{
-        title: "System",
-        description: "",
-        color: 16754756,
-        footer: {
-          text: `Lobby ID: ${client.lobbyId} - Online Players: ${client.players.length} - Skribbl-Relay`
-        }
-      }]
-    }
+      embeds: [
+        {
+          title: "System",
+          description: "",
+          color: 16754756,
+          footer: {
+            text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`,
+          },
+          thumbnail: {
+            url: "",
+          },
+        },
+      ],
+    };
 
-    switch(client.state) {
+    switch (client.state) {
       case 0:
         message.embeds[0].description = "Waiting for players";
         client.currentDrawer = null;
@@ -234,26 +268,35 @@ function main() {
         break;
 
       case 3:
-        if(client.options.name === client.currentDrawer?.name) break;
+        if (client.options.name === client.currentDrawer?.name) break;
 
-        message.embeds[0].description = `**${client.currentDrawer?.name ?? "N/A"}** is choosing a word!`;
+        message.embeds[0].description = `**${
+          client.currentDrawer?.name ?? "N/A"
+        }** is choosing a word!`;
+        message.embeds[0].thumbnail.url =
+          "https://skribbl.io/img/randomize.gif";
         break;
 
       case 4:
-        if(client.options.name === client.currentDrawer?.name) break;
+        if (client.options.name === client.currentDrawer?.name) break;
 
-        message.embeds[0].description = `**${client.currentDrawer?.name ?? "N/A"}** is drawing now!`;
+        message.embeds[0].description = `**${
+          client.currentDrawer?.name ?? "N/A"
+        }** is drawing now!`;
+        message.embeds[0].thumbnail.url = "https://i.ibb.co/k4vb1PM/pen-2.gif";
         message.embeds[0].color = 3765710;
         break;
 
       case 5:
-        switch(data.data.reason) {
+        switch (data.data.reason) {
           case 0:
             message.embeds[0].description = `The word was '**${data.data.word}**'\nEveryone guessed the word!`;
             break;
 
           case 1:
             message.embeds[0].description = `Time is up!\nThe word was '**${data.data.word}**'`;
+            message.embeds[0].thumbnail.url =
+              "https://skribbl.io/img/setting_2.gif";
             break;
 
           case 2:
@@ -266,99 +309,130 @@ function main() {
         break;
 
       case 7:
-        message.embeds[0].description = 'Waiting for the game to start';
+        message.embeds[0].description = "Waiting for the game to start";
         break;
     }
 
-    console.log(message.embeds[0].description)
+    if (message.embeds[0].description === "") return;
+
     send(message);
   });
 
   client.on("startError", (gameStartErr) => {
     send({
       username: "Skribbl-Relay",
-      embeds: [{
-        title: "System",
-        description: gameStartErr === 0 ? "The host needs atleast **2** players to start the game" : "The server will be restarting soon",
-        color: 16711680,
-        footer: {
-          text: `Lobby ID: ${client.lobbyId} - Online Players: ${client.players.length} - Skribbl-Relay`
-        }
-      }]
+      embeds: [
+        {
+          title: "System",
+          description:
+            gameStartErr === 0
+              ? "The host needs atleast **2** players to start the game"
+              : "The server will be restarting soon",
+          color: 16711680,
+          footer: {
+            text: `Lobby ID: ${client.lobbyId} - Online Players: ${client.players.length} - Skribbl-Relay`,
+          },
+        },
+      ],
     });
-  })
+  });
 
-  client.on("playerGuessed", ({player}) => {
+  client.on("playerGuessed", ({ player }) => {
     send({
       username: "Skribbl-Relay",
-      embeds: [{
-        title: "System",
-        description: `**${player.name}** guessed the word!`,
-        color: 65280,
-        footer: {
-          text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`
-        }
-      }]
+      embeds: [
+        {
+          title: "System",
+          description: `**${player.name}** guessed the word!`,
+          color: 65280,
+          footer: {
+            text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`,
+          },
+        },
+      ],
     });
-  })
+  });
 
   client.on("hintRevealed", () => {
     send({
       username: "Skribbl-Relay",
-      embeds: [{
-        title: "System",
-        description: `A hint was revealed!`,
-        color: 16754756,
-        footer: {
-          text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`
-        }
-      }]
+      embeds: [
+        {
+          title: "System",
+          description: `A hint was revealed!`,
+          color: 16754756,
+          footer: {
+            text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`,
+          },
+          thumbnail: {
+            url: "https://skribbl.io/img/setting_5.gif",
+          },
+        },
+      ],
     });
-  })
+  });
 
   client.on("roundStart", () => {
     send({
       username: "Skribbl-Relay",
-      embeds: [{
-        title: "System",
-        description: `Round **${client.round}** has started`,
-        color: 65280,
-        footer: {
-          text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`
-        }
-      }]
+      embeds: [
+        {
+          title: "System",
+          description: `Round **${client.round}** has started`,
+          color: 65280,
+          footer: {
+            text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`,
+          },
+        },
+      ],
     });
   });
 
-  client.on("vote", ({player, vote}) => {
-        send({
-            username: "Skribbl-Relay",
-            embeds: [{
-                title: "System",
-                description: `**${player.name}** ${vote === 0 ? "disliked" : "liked"} the drawing!`,
-                color: vote === 0 ? 16711680 : 65280,
-                footer: {
-                    text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`
-                }
-            }]
-        });
+  client.on("vote", ({ player, vote }) => {
+    send({
+      username: "Skribbl-Relay",
+      embeds: [
+        {
+          title: "System",
+          description: `**${player.name}** ${
+            vote === 0 ? "disliked" : "liked"
+          } the drawing!`,
+          color: vote === 0 ? 16711680 : 65280,
+          footer: {
+            text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`,
+          },
+          thumbnail: {
+            url:
+              vote === 0
+                ? "https://i.ibb.co/Fgg0h1P/thumbsdown.gif"
+                : "https://i.ibb.co/Ch2PW4B/thumbsup.gif",
+          },
+        },
+      ],
     });
+  });
 
-  client.on("text", ({player, msg}) => {
-    if(player.name === client.options.name) return;
+  client.on("text", ({ player, msg }) => {
+    if (player.name === client.options.name) return;
 
-    const didGuess = player.name === client.currentDrawer?.name || player.guessed;
+    const didGuess =
+      player.name === client.currentDrawer?.name || player.guessed;
 
     send({
       username: "Skribbl-Relay",
-      embeds: [{
-        title: "Chat",
-        description: `**${player.name}**: ${msg}`,
-        color: didGuess ? 8236351 : 16777215,
-        footer: {
-          text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`
-        }
-      }]
+      embeds: [
+        {
+          title: "Chat",
+          description: `**${player.name}**: ${msg}`,
+          color: didGuess ? 8236351 : 16777215,
+          footer: {
+            text: `Lobby ID: ${client.lobbyId} - Skribbl-Relay`,
+          },
+          thumbnail: {
+            url: "https://skribbl.io/img/setting_0.gif",
+          },
+        },
+      ],
     });
   });
 }
